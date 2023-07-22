8f43a17cae273b3cafbd073e2bd43f676eced9cf
TensorNd <do-not-mail@nothing.com>
2023-07-22 13:57:16 +0530

    Added the forward method of the network.

--- Network_Architecture.py
+++ Network_Architecture.py
@@ -1,8 +1,119 @@
-from torch.nn import Module 
+from torch.nn import Module, Conv2d, MaxPool2d, Flatten, Linear, ReLU, Tanh 
 
 class Network(Module):
-    def __init__(self):
-        pass
+    def __init__(self, config):
+        super(Network, self).__init__()
+        self.config = config
+        if self.config['NetworkArchitecture']['type'] == 'CNN':
+            # Assuming input is C-channel image of the environment state.
+            # Expected Image dimensions -> C X 128 X 128 
+            self.cnn_1 = Conv2d(
+                in_channels = self.config['NetworkArchitecture']['n_channels'], 
+                out_channels = 32, 
+                kernel_size = (3,3),
+                padding = 1,
+                dialation = 1 
+            )
+            self.cnn_2 = Conv2d(
+                in_channels = 32, 
+                out_channels = 32, 
+                kernel_size = (3,3),
+                padding = 1,
+                dialation = 1 
+            ) 
+            self.max_pool = MaxPool2d(
+                kernel_size = (2,2)
+            )
+            self.cnn_3 = Conv2d(
+                in_channels = 32, 
+                out_channels = 64, 
+                kernel_size = (3,3),
+                padding = 1,
+                dialation = 1 
+            )
+            self.cnn_4 = Conv2d(
+                in_channels = 64, 
+                out_channels = 64, 
+                kernel_size = (3,3),
+                padding = 1,
+                dialation = 1 
+            )
+            self.cnn_5 = Conv2d(
+                in_channels = 64, 
+                out_channels = 128, 
+                kernel_size = (3,3),
+                padding = 0,
+                dialation = 1 
+            )
+            self.cnn_6 = Conv2d(
+                in_channels = 128, 
+                out_channels = 128, 
+                kernel_size = (3,3),
+                padding = 0,
+                dialation = 1 
+            )
+            self.cnn_7 = Conv2d(
+                in_channels = 128, 
+                out_channels = 256, 
+                kernel_size = (3,3),
+                padding = 0,
+                dialation = 1 
+            )
+            self.cnn_8 = Conv2d(
+                in_channels = 256, 
+                out_channels = 256, 
+                kernel_size = (3,3),
+                padding = 0,
+                dialation = 1 
+            )
+            self.flat = Flatten()
+            self.dense_1 = Linear(256 * 5 * 5, 256)
+            self.dense_2 = Linear(256, 128)
+            self.dense_3 = Linear(128, 64)
+            self.dense_4 = Linear(64, 32)
+            self.dense_5 = Liner(32, 16)
+            self.dense_6 = Linear(16, 8)
+            self.dense_7 = Linear(8, 1)
+        else:
+            raise Exception(f"[Exception-NA001] - {self.config['NetworkArchitecture']['type']} Architecture type is not supported!")
+    def forward(self, x):
+        assert x.shape[0] == self.config['NetworkArchitecture']['n_channels'], "Input data contains different number of channels!" 
+        assert (x.shape[1] == x.shape[2]) and (x.shape[2] == 128), "Height and width of the array is not 128!"
+        
+        x = ReLU()(self.cnn_1(x)) # (C, 128, 128) -> (32, 128, 128)
+        x = Dropout(p = self.config['NetworkArchitecture']['p_dropout'])(x) 
+        x = ReLU()(self.cnn_2(x)) # (32, 128, 128) -> (32, 128, 128)
+        x = self.max_pool() # (32, 128, 128) -> (32, 64, 64)
+        x = ReLU()(self.cnn_3(x)) # (32, 64, 64) -> (64, 64, 64)
+        x = Dropout(p = self.config['NetworkArchitecture']['p_dropout'])(x)
+        x = ReLU()(self.cnn_4(x)) # (64, 64, 64) -> (64, 64, 64)
+        x = self.max_pool() # (64, 64, 64) -> (64, 32, 32)
+        x = ReLU()(self.cnn_5(x)) # (64, 32, 32) -> (128, 30, 30)
+        x = Dropout(p = self.config['NetworkArchitecture']['p_dropout'])(x)
+        x = ReLU()(self.cnn_6(x)) # (128, 30, 30) -> (128, 28, 28)
+        x = self.max_pool() # (128, 28, 28) -> (128, 14, 14)
+        x = ReLU()(self.cnn_7(x)) # (128, 14, 14) -> (256, 7, 7)
+        x = Dropout(p = self.config['NetworkArchitecture']['p_dropout'])(x)
+        x = ReLU()(self.cnn_8(x)) # (256, 7, 7) -> (256, 5, 5)
+        x = self.flat(x) # (256, 5, 5) -> (256 * 5 * 5)
+        x = ReLU()(self.dense_1(x)) # (256 * 5 * 5) -> 256
+        x = Dropout(p = self.config['NetworkArchitecture']['p_dropout'])(x)
+        x = ReLU()(self.dense_2(x)) # 256 -> 128
+        x = Dropout(p = self.config['NetworkArchitecture']['p_dropout'])(x)
+        x = ReLU()(self.dense_3(x)) # 128 -> 64
+        x = Dropout(p = self.config['NetworkArchitecture']['p_dropout'])(x)
+        x = ReLU()(self.dense_4(x)) # 64 -> 32
+        x = Dropout(p = self.config['NetworkArchitecture']['p_dropout'])(x)
+        x = ReLU()(self.dense_5(x)) # 32 -> 16
+        x = Dropout(p = self.config['NetworkArchitecture']['p_dropout'])(x)
+        x = ReLU()(self.dense_6(x)) # 16 -> 8
+        x = Dropout(p = self.config['NetworkArchitecture']['p_dropout'])(x)
+        x = self.dense_7(x) # 8 -> 1
+        if self.config['NetworkArchitecture']['output_activation'] == 'relu':
+            x = ReLU()(x)
+        else:
+            x = Tanh(x)
+        return x
     def update(self):
         pass
     def update_parameters(self, TAU, parameters):

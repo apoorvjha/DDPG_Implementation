13220636269549371293c85a8fe2280516818160
TensorNd <do-not-mail@nothing.com>
2023-07-22 22:19:17 +0530

    Added the appropriate object instatiation parameters for the model.

--- Main.py
+++ Main.py
@@ -7,13 +7,15 @@ import environment
 if __name__ == '__main__':
     # Read configuration
     config = utility.read_configuration()
+    # Get device
+    device = utility.get_device()
     # Initializing Critic Network
-    critic_network = Network_Architecture.Network(...)
+    critic_network = Network_Architecture.Network(config, mode = 'critic').to(device)
     # Initializing Actor Network
-    actor_network = Network_Architecture.Network(...)
+    actor_network = Network_Architecture.Network(config).to(device)
     # Initializing Target counterparts of Actor and Critic Networks (Enhanced Learning Stability)
-    critic_target_network = Network_Architecture.Network(...)
-    actor_target_network = Network_Architecture.Network(...)
+    critic_target_network = Network_Architecture.Network(config, mode = 'critic').to(device)
+    actor_target_network = Network_Architecture.Network(config).to(device)
     # Initializing Experience Replay Buffer
     buffer = Experience_Replay.ExperienceReplyBuffer(...)
     # Initilize the Simulation Environment
@@ -40,13 +42,13 @@ if __name__ == '__main__':
                 # Get Minibatch data from buffer
                 current_states, current_actions, next_states, rewards = buffer.sample(config["experience_replay_buffer_cutoff"])
                 # Get Future Reward estimate from Bellman Equation
-                Y = rewards + config["GAMMA"] * critic_target_network([next_states, next_actions])
+                Y = rewards + config["GAMMA"] * critic_target_network(next_states, next_actions)
                 # Update the weights of Critic Network (Squared Mean Loss) 
-                critic_network.update(Y, critic_network([current_states, current_actions]))
+                critic_network.update(Y, critic_network(current_states, current_actions))
                 # Update Actor Network (Mean of Sampled Policy Gradients)
-                actor_network.update(critic_network([current_states, current_actions]))
+                actor_network.update(critic_network(current_states, current_actions))
                 # Update the target Networks
-                critic_target_network.update_parameters(config["TAU"], critic_network.parameters())
-                actor_target_network.update_parameters(config["TAU"], actor_network.parameters())
+                critic_target_network.update_parameters(config["TAU"], critic_network.state_dict())
+                actor_target_network.update_parameters(config["TAU"], actor_network.state_dict())
             # Increment Timestep variable
             T += 1

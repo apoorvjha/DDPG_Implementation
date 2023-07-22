c6b92a6d0e595ed0dfd4d3169ebff7785e2a50ba
TensorNd <do-not-mail@nothing.com>
2023-07-22 13:56:41 +0530

    Implemented the Ornstein Ulembeck Noise generation process.

--- Noise_Perturbation.py
+++ Noise_Perturbation.py
@@ -1,7 +1,29 @@
-
+import numpy as np
 
 class OrnsteinUlembeckNoise:
-    def __init__(self):
-        pass
+    """
+    To implement the better exploration by actor network, This function creates noisy
+    perturbations. It samples noise from normal correlated distribution.
+
+    Code(Ref) : https://keras.io/examples/rl/ddpg_pendulum/  
+    """
+    def __init__(self, mean, std, theta = 0.15, dt = 1e-2, x_initial = None):
+        self.theta = theta
+        self.mean = mean
+        self.std = std
+        self.dt = dt
+        self.x_initial = x_initial
+        self.reset()
     def sample(self):
-        pass
+        x = (
+            self.x_previous
+            + self.theta * (self.mean - self.x_previous) * self.dt
+            + self.std * np.sqrt(self.dt) * np.random.normal(size = self.mean.shape)
+        )
+        self.x_previous = x
+        return x
+    def reset(self):
+        if self.x_initial is not None:
+            x_previous = self.x_initial
+        else:
+            self.x_previous = np.zeros_like(self.mean)

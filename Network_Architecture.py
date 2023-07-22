f050f714f54fca7a3bed65444a3d6a7514546440
TensorNd <do-not-mail@nothing.com>
2023-07-23 00:46:07 +0530

    Updated the code to fix the interim bugs and issues.

--- Network_Architecture.py
+++ Network_Architecture.py
@@ -1,5 +1,6 @@
-from torch.nn import Module, Conv2d, MaxPool2d, Flatten, Linear, ReLU, Tanh, MSELoss
+from torch.nn import Module, Conv2d, MaxPool2d, Flatten, Linear, ReLU, Tanh, MSELoss, Dropout
 from torch.optim import Adam 
+import torch
 
 class Network(Module):
     def __init__(self, config, mode = 'actor'):
@@ -13,14 +14,14 @@ class Network(Module):
                 out_channels = 32, 
                 kernel_size = (3,3),
                 padding = 1,
-                dialation = 1 
+                dilation = 1 
             )
             self.cnn_2 = Conv2d(
                 in_channels = 32, 
                 out_channels = 32, 
                 kernel_size = (3,3),
                 padding = 1,
-                dialation = 1 
+                dilation = 1 
             ) 
             self.max_pool = MaxPool2d(
                 kernel_size = (2,2)
@@ -30,52 +31,52 @@ class Network(Module):
                 out_channels = 64, 
                 kernel_size = (3,3),
                 padding = 1,
-                dialation = 1 
+                dilation = 1 
             )
             self.cnn_4 = Conv2d(
                 in_channels = 64, 
                 out_channels = 64, 
                 kernel_size = (3,3),
                 padding = 1,
-                dialation = 1 
+                dilation = 1 
             )
             self.cnn_5 = Conv2d(
                 in_channels = 64, 
                 out_channels = 128, 
                 kernel_size = (3,3),
                 padding = 0,
-                dialation = 1 
+                dilation = 1 
             )
             self.cnn_6 = Conv2d(
                 in_channels = 128, 
                 out_channels = 128, 
                 kernel_size = (3,3),
                 padding = 0,
-                dialation = 1 
+                dilation = 1 
             )
             self.cnn_7 = Conv2d(
                 in_channels = 128, 
                 out_channels = 256, 
                 kernel_size = (3,3),
                 padding = 0,
-                dialation = 1 
+                dilation = 1 
             )
             self.cnn_8 = Conv2d(
                 in_channels = 256, 
                 out_channels = 256, 
                 kernel_size = (3,3),
                 padding = 0,
-                dialation = 1 
+                dilation = 1 
             )
             self.flat = Flatten()
             if mode != 'actor':
-                self.dense_1 = Linear(256 * 5 * 5 + self.config['n_actions'], 256)
+                self.dense_1 = Linear(256 * 10 * 10 + self.config['n_actions'], 256)
             else:    
-                self.dense_1 = Linear(256 * 5 * 5, 256)
+                self.dense_1 = Linear(256 * 10 * 10, 256)
             self.dense_2 = Linear(256, 128)
             self.dense_3 = Linear(128, 64)
             self.dense_4 = Linear(64, 32)
-            self.dense_5 = Liner(32, 16)
+            self.dense_5 = Linear(32, 16)
             self.dense_6 = Linear(16, 8)
             self.dense_7 = Linear(8, 1)
         else:
@@ -87,23 +88,23 @@ class Network(Module):
         x = ReLU()(self.cnn_1(x)) # (C, 128, 128) -> (32, 128, 128)
         x = Dropout(p = self.config['NetworkArchitecture']['p_dropout'])(x) 
         x = ReLU()(self.cnn_2(x)) # (32, 128, 128) -> (32, 128, 128)
-        x = self.max_pool() # (32, 128, 128) -> (32, 64, 64)
+        x = self.max_pool(x) # (32, 128, 128) -> (32, 64, 64)
         x = ReLU()(self.cnn_3(x)) # (32, 64, 64) -> (64, 64, 64)
         x = Dropout(p = self.config['NetworkArchitecture']['p_dropout'])(x)
         x = ReLU()(self.cnn_4(x)) # (64, 64, 64) -> (64, 64, 64)
-        x = self.max_pool() # (64, 64, 64) -> (64, 32, 32)
+        x = self.max_pool(x) # (64, 64, 64) -> (64, 32, 32)
         x = ReLU()(self.cnn_5(x)) # (64, 32, 32) -> (128, 30, 30)
         x = Dropout(p = self.config['NetworkArchitecture']['p_dropout'])(x)
         x = ReLU()(self.cnn_6(x)) # (128, 30, 30) -> (128, 28, 28)
-        x = self.max_pool() # (128, 28, 28) -> (128, 14, 14)
-        x = ReLU()(self.cnn_7(x)) # (128, 14, 14) -> (256, 7, 7)
+        x = self.max_pool(x) # (128, 28, 28) -> (128, 14, 14)
+        x = ReLU()(self.cnn_7(x)) # (128, 14, 14) -> (256, 12, 12)
         x = Dropout(p = self.config['NetworkArchitecture']['p_dropout'])(x)
-        x = ReLU()(self.cnn_8(x)) # (256, 7, 7) -> (256, 5, 5)
-        x = self.flat(x) # (256, 5, 5) -> (256 * 5 * 5)
+        x = ReLU()(self.cnn_8(x)) # (256, 12, 12) -> (256, 10, 10)
+        x = self.flat(x) # (256, 10, 10) -> (256 * 10 * 10)
         if x_action is not None:
-            x = torch.cat((ReLU()(self.dense_1(x)), x_action), dim = 0) # (256 * 5 * 5 + 1) -> 256
+            x = torch.cat((ReLU()(self.dense_1(x)), x_action), dim = 0) # (256 * 10 * 10 + 1) -> 256
         else:
-            x = ReLU()(self.dense_1(x)) # (256 * 5 * 5) -> 256
+            x = ReLU()(self.dense_1(x)) # (256 * 10 * 10) -> 256
         x = Dropout(p = self.config['NetworkArchitecture']['p_dropout'])(x)
         x = ReLU()(self.dense_2(x)) # 256 -> 128
         x = Dropout(p = self.config['NetworkArchitecture']['p_dropout'])(x)
@@ -131,7 +132,8 @@ class Network(Module):
             optimizer.step()
         elif len(args) == 1:
             # Mean of supplied argument as loss
-            loss = -1 * torch.mean(args[-1])
+            policy_gradient = -1 * torch.mean(args[-1])
+            loss = policy_gradient.clone().detach().requires_grad_(True)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
@@ -139,4 +141,4 @@ class Network(Module):
             raise Exception("[Exception-NA002] - The supplied number of arguments is incorrect!")
     def update_parameters(self, TAU, parameters):
         for param in self.state_dict().keys():
-            self.state_dict()[param][0] = self.state_dict()[param] * TAU + (1 - TAU) * parameters[param][0]
+            self.state_dict()[param] = self.state_dict()[param] * TAU + (1 - TAU) * parameters[param]
